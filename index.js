635d1d0d9f00ed5e3fb04ad4f46a299b923d2a63
SyedFarooqAlii <syedfarooqali15@gmail.com>
2024-04-14 21:59:49 +0500

    This is a currency converter app

---
+++ index.js
@@ -0,0 +1,40 @@
+#! user/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.cyanBright.bold("\n \tWelcome To My Currency Convertor App"));
+let currency = {
+    USD: 1,
+    PKR: 277.70,
+    INR: 84,
+    EUR: 0.92,
+    SOM: 89.34,
+    DINAR: 0.31 //Kuwaiti Dinar
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: chalk.blueBright('Select your currency.'),
+        type: 'list',
+        choices: ['USD', 'PKR', 'INR', 'EUR', 'SOM', 'DINAR']
+    },
+    {
+        name: 'amount',
+        message: chalk.gray('How much ammount you want to convert?'),
+        type: 'number'
+    },
+    {
+        name: 'to',
+        message: chalk.gray('Select currency you want to convert.'),
+        type: 'list',
+        choices: ['USD', 'PKR', 'INR', 'EUR', 'SOM', 'DINAR']
+    }
+]);
+//perform currency convertor by using formula
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+//formula of conversion
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+//dispaly the converted amount
+console.log(`converted Amount =${chalk.green(convertedAmount.toFixed(2))}`);
---
+++ index.ts
@@ -0,0 +1,44 @@
+#! user/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.cyanBright.bold(" Welcome To My Currency Convertor App"));
+
+let currency: any = {
+    USD: 1, // Base Currency || US Dollar
+    PKR: 277.70,//Pakistani Rupee
+    INR: 84,//Indian rupee
+    EUR: 0.92,//Euro
+    SOM: 89.34, //Kyrgystani Som
+    DINAR: 0.31 //Kuwaiti Dinar
+};
+
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: 'from',
+            message: chalk.blueBright('Select your currency.'),
+            type: 'list',
+            choices: ['USD', 'PKR', 'INR', 'EUR', 'SOM', 'DINAR']
+        },
+        {
+            name: 'amount',
+            message: chalk.gray('How much ammount you want to convert?'),
+            type: 'number'
+        },
+        {
+            name: 'to',
+            message: chalk.gray('Select currency you want to convert.'),
+            type: 'list',
+            choices: ['USD', 'PKR', 'INR', 'EUR', 'SOM', 'DINAR']
+        }
+        
+    ])
+//perform currency convertor by using formula
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+//formula of conversion
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+//dispaly the converted amount
+console.log(`converted Amount =${ chalk.green (convertedAmount.toFixed(2))}`);
